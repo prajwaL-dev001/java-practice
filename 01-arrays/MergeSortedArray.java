7d8b214eed607675e8df26558b79861bec65d179
Prajwal N <228729514+prajwaL-dev001@users.noreply.github.com>
2026-05-17 00:29:28 +0530

    add DSA problems: 3sum, contains duplicate, rotate image

--- 01-arrays/MergeSortedArray.java
+++ 01-arrays/MergeSortedArray.java
@@ -2,9 +2,9 @@ public class MergeSortedArray {
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int i = m - 1;        // last element of nums1
-        int j = n - 1;        // last element of nums2
-        int k = m + n - 1;    // last position of nums1
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
         while (i >= 0 && j >= 0) {
 
@@ -18,7 +18,6 @@ public class MergeSortedArray {
             k--;
         }
 
-        // if nums2 still has elements
         while (j >= 0) {
             nums1[k] = nums2[j];
             j--;
---
+++ RotateImage.java
@@ -0,0 +1,47 @@
+public class RotateImage {
+
+    public static void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+
+        // Step 1: transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        rotate(matrix);
+
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
