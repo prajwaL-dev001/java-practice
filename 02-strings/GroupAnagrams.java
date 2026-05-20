66a51c1d5cd2545dcb3718aad2d60a8dabb62d4e
Prajwal N <228729514+prajwaL-dev001@users.noreply.github.com>
2026-05-20 21:18:56 +0530

    Added group anagrams problem

---
+++ 02-strings/GroupAnagrams.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+
+            char[] chars = word.toCharArray();
+
+            Arrays.sort(chars);
+
+            String sorted = new String(chars);
+
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<>());
+            }
+
+            map.get(sorted).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        System.out.println(groupAnagrams(strs));
+    }
+}
