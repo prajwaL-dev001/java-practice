23d93589e0b0b3c8876c506a9beaf7a4a2e6e1f0
Prajwal N <228729514+prajwaL-dev001@users.noreply.github.com>
2026-05-22 01:56:49 +0530

    Added palindrome number problem

---
+++ 02-strings/PalindromeNumber.java
@@ -0,0 +1,26 @@
+class PalindromeNumber {
+
+    public static boolean isPalindrome(int x) {
+
+        int original = x;
+        int reverse = 0;
+
+        while (x > 0) {
+
+            int digit = x % 10;
+
+            reverse = reverse * 10 + digit;
+
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+
+        int x = 121;
+
+        System.out.println(isPalindrome(x));
+    }
+}
