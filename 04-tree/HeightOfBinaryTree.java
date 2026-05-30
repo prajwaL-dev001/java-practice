4bc221118cc20eb560d6c6777b9b5df3ede4f919
Prajwal N <228729514+prajwaL-dev001@users.noreply.github.com>
2026-05-30 20:51:07 +0530

    added height of binary tree problem

---
+++ 04-tree/HeightOfBinaryTree.java
@@ -0,0 +1,38 @@
+class HeightOfBinaryTree {
+
+    static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static int height(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.left.left.left = new TreeNode(5);
+
+        System.out.println(height(root));
+    }
+}
