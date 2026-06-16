6cbfabf5b0397cb926fc9143fbbaf28521ed8522
Prajwal N <228729514+prajwaL-dev001@users.noreply.github.com>
2026-06-16 21:45:51 +0530

    added gitignore and add code

--- 05-stack/QueueFrontElementFinder.java
+++ 05-stack/QueueFrontElementFinder.java
@@ -2,18 +2,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class QueueFrontElementFinder {
-
     public static void main(String[] args) {
-
         Queue<Integer> studentDataQueue = new LinkedList<>();
-
         studentDataQueue.add(100);
         studentDataQueue.add(200);
         studentDataQueue.add(300);
         studentDataQueue.add(400);
-
         System.out.println("Queue Elements: " + studentDataQueue);
-
         System.out.println("Front Element: " + studentDataQueue.peek());
     }
 }
